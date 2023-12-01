92b24c88ff3987c923ea7d6dbe56cdf480f469b7
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2023-12-01 14:20:14 +0530

    Your commit message

--- src/home/user/userdisplay.js
+++ src/home/user/userdisplay.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import './Userdisplay.css'; // Import the CSS file
+import './user.css'; // Import the CSS file
 import { Link, useParams } from 'react-router-dom'; // Import Link from React Router
 import { Axios } from 'axios';
 
