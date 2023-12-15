0a41662861c04544077a3b74e0155d94c521408e
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2023-12-15 23:30:35 +0530

    h

--- src/home/user/template1/template1inprofile.js
+++ src/home/user/template1/template1inprofile.js
@@ -218,7 +218,7 @@ useEffect(() => {
         to={`${Linkno(props)}/${props.shop_id}/${props.shop_name}`}
         style={{ textDecoration: 'none', color: 'black' }}
       >
-              {props.shop_name}
+             <p style={{color: 'white'}}>{props.shop_name}</p> 
             </Link>
             <Link to={`/user/${props.user_id}`}  style={{ textDecoration: 'none', color: 'black' }}> 
             <span className="shop-owner">Shop by {userInfo[0]?.first_name}</span>
