fe2e80510e2c32768da3237246fe05c256380de1
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-01-07 15:51:29 +0530

    m

--- src/home/mainpage/mainpage.js
+++ src/home/mainpage/mainpage.js
@@ -1,29 +1,18 @@
-import React,{Fragment, useState, useEffect} from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import dropment from '../header/images/drop2_logo.png'
 import './mainpage.css'
 import AllTemplate1app from "./template1/template1app";
-import Users from "../user/userapp";
-import ChatMessageapp from "../chat/chat";
-import Stories from "../stories/stories";
-import Storiesapp from "../stories/storiesdisplay";
 import NotificationComponent from "../notifications/notifications";
-import userPic from '../header/images/icons8-male-user-30 (1).png'
 import notification from '../header/images/icons8-notifications-78.png'
-import message from '../header/images/icons8-messages-48.png'
-import search from '../header/images/icons8-search-50.png'
 import order from '../header/images/icons8-logistics-32.png'
 import profile from '../header/images/icons8-male-user-24.png'
-import back from '../header/images/icons8-back-24.png'
-import addshop from '../header/images/icons8-add-24.png'
-import { useMediaQuery } from 'react-responsive';
-import Template1inmain from "./template1/template1inprofile";
-
+import search from '../header/images/icons8-search-50.png'
 
 const Mainpage = () => {
   const [auth, setAuth] = useState(false);
   const nav = useNavigate();
-  const [showNotification, setShowNotification] = useState(false); // State for showing/hiding notification
+  const [showNotification, setShowNotification] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -32,7 +21,7 @@ const Mainpage = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
-    setAuth(false); // Update auth state after logout
+    setAuth(false);
   };
 
   useEffect(() => {
@@ -44,69 +33,67 @@ const Mainpage = () => {
   }, []);
 
   const toggleNotification = () => {
-    setShowNotification(!showNotification); // Toggle notification display
-  }
+    setShowNotification(prevState => !prevState);
+  };
 
-
-    return<Fragment>
-  <nav className="navbar-dropment-main-page">
-      <img src={dropment} alt="Dropment" className="logo-dropment-main-page"/> 
-      <nav className="big-screen-nav-bar-dropment-main-page">
-      <Link to='/profile' style={{textDecoration: 'none'}}>
-      <button>My Profile <img src={profile}/></button>
-      </Link>
-      <Link to='/orders' style={{textDecoration: 'none'}}>
-      <button>My Orders <img src={order}/></button>
-      </Link>
-      <Link to='/overview' style={{textDecoration: 'none'}}>
-      <button>Overview</button>
-      </Link>
-      <Link to='/search' style={{textDecoration: 'none'}}>
-      <button>Search <img src={search}/></button>
-      </Link>
-      <button>Notification <img src={notification}/></button>
-      {auth ? (
-        <button onClick={handleLogout}>
-          Logout
-        </button>
-      ) : (
-        <Link to="/login" style={{textDecoration: 'none'}}>
-          <button>Login</button>
-        </Link>
-      )}
-    </nav>
-      <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
-        <Link to='/profile' style={{textDecoration: 'none'}}>
-      <button>My Profile <img src={profile}/></button>
-      </Link>
-      <Link to='/orders' style={{textDecoration: 'none'}}>
-      <button>My Orders <img src={order}/></button>
-      </Link>
-      <Link to='/overview' style={{textDecoration: 'none'}}>
-      <button>Overview</button>
-      </Link>
-      <Link to='/search' style={{textDecoration: 'none'}}>
-      <button>Search <img src={search}/></button>
-      </Link>
-      <button>Notification <img src={notification}/></button>
-      {auth ? (
-        <button onClick={handleLogout}>
-          Logout
-        </button>
-      ) : (
-        <Link to="/login" style={{textDecoration: 'none'}}>
-          <button>Login</button>
-        </Link>
-      )}
+  return (
+    <Fragment>
+      <nav className="navbar-dropment-main-page">
+        <img src={dropment} alt="Dropment" className="logo-dropment-main-page"/> 
+        <nav className="big-screen-nav-bar-dropment-main-page">
+          <Link to='/profile' style={{textDecoration: 'none'}}>
+            <button>My Profile <img src={profile}/></button>
+          </Link>
+          <Link to='/orders' style={{textDecoration: 'none'}}>
+            <button>My Orders <img src={order}/></button>
+          </Link>
+          <Link to='/search' style={{textDecoration: 'none'}}>
+            <button>Search <img src={search}/></button>
+          </Link>
+          <button onClick={toggleNotification}>Notification <img src={notification}/></button>
+          {auth ? (
+            <button onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <Link to="/login" style={{textDecoration: 'none'}}>
+              <button>Login</button>
+            </Link>
+          )}
+        </nav>
+        <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+          <Link to='/profile' style={{textDecoration: 'none'}}>
+            <button>My Profile <img src={profile}/></button>
+          </Link>
+          <Link to='/orders' style={{textDecoration: 'none'}}>
+            <button>My Orders <img src={order}/></button>
+          </Link>
+          <Link to='/search' style={{textDecoration: 'none'}}>
+            <button>Search <img src={search}/></button>
+          </Link>
+          <button onClick={toggleNotification}>Notification <img src={notification}/></button>
+          {auth ? (
+            <button onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <Link to="/login" style={{textDecoration: 'none'}}>
+              <button>Login</button>
+            </Link>
+          )}
+        </div>
+        <div className="hamburger-dropment-main-page" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </nav>
+      <div className={`notification ${showNotification ? 'show' : 'hide'}`}>
+        <NotificationComponent/>
       </div>
-      <div className="hamburger-dropment-main-page" onClick={toggleMenu}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </div>
-    </nav>
-    <AllTemplate1app/>
-  </Fragment>
-}
+      <AllTemplate1app/>
+    </Fragment>
+  );
+};
 
-export default Mainpage
+export default Mainpage;
